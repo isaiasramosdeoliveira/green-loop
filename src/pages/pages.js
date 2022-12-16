853bd1396fc84baac48b3a4cf3f0c185ac48c717
Isaías ramos <isaiasramosdeoliverira2003@gmail.com>
2022-12-16 02:47:29 -0300

    Acréscimo dos campos de login e planos.

--- src/pages/pages.js
+++ src/pages/pages.js
@@ -3,15 +3,100 @@ export const loader = `
 `;
 
 export const plans = `
-  <section id="plans">
-
-  </section>
+<section id="plans">
+<div class="card">
+  <h4>1 x 6</h4>
+  <small>Local da plantação:</small>
+  <div>
+    <picture>
+      <img src="src/assets/img/plans_1x1.png" alt="Plano">
+    </picture>
+    <h2>R$200.00</h2>
+  </div>
+  <button>Contratar serviços</button>
+</div>
+<div class="card">
+  <h4>2 x 6</h4>
+  <small>Local da plantação:</small>
+  <div>
+    <picture>
+      <img src="src/assets/img/plans_1x2.png" alt="Plano">
+    </picture>
+    <h2>R$400.00</h2>
+  </div>
+  <button>Contratar serviços</button>
+</div>
+<div class="card">
+  <h4>3 x 6</h4>
+  <small>Local da plantação:</small>
+  <div>
+    <picture>
+      <img src="src/assets/img/plans_1x3.png" alt="Plano">
+    </picture>
+    <h2>R$650.00</h2>
+  </div>
+  <button>Contratar serviços</button>
+</div>
+<div class="card">
+  <h4>4 x 6</h4>
+  <small>Local da plantação:</small>
+  <div>
+    <picture>
+      <img src="src/assets/img/plans_1x4.png" alt="Plano">
+    </picture>
+    <h2>R$800.00</h2>
+  </div>
+  <button>Contratar serviços</button>
+</div>
+<div class="card">
+  <h4>5 x 6</h4>
+  <small>Local da plantação:</small>
+  <div>
+    <picture>
+      <img src="src/assets/img/plans_1x5.png" alt="Plano">
+    </picture>
+    <h2>R$950.00</h2>
+  </div>
+  <button>Contratar serviços</button>
+</div>
+<div class="card">
+  <h4>6 x 6</h4>
+  <small>Local da plantação:</small>
+  <div>
+    <picture>
+      <img src="src/assets/img/plans_1x6.png" alt="Plano">
+    </picture>
+    <h2>R$1450.00</h2>
+  </div>
+  <button>Contratar serviços</button>
+</div>
+</section>
+</section>
 `;
 
 export const login = `
-  <section id="login">
-
-  </section>
+<section id="login">
+<form>
+  <label for="name">
+    <input placeholder="Nome" type="text" name="name" id="name">
+  </label>
+  <br>
+  <label for="email">
+    <input placeholder="E-mail" type="email" name="email" id="email">
+  </label>
+  <br>
+  <label for="password">
+    <input placeholder="Senha" type="password" name="password" id="password">
+  </label>
+  <br>
+  <label for="confirmEmail">
+    <input placeholder="Confirmação do E-mail" type="email" name="confirmEmail" id="confirmEmail">
+  </label>
+  <br>
+  <button>Cadastrar</button>
+  <small>Já tem conta? <a href="">Entrar</a></small>
+</form>
+</section>
 `;
 export const aboutUs = `
   <section id="aboutUs">
@@ -27,6 +112,6 @@ export const cart = `
 
 export const contacts = `
   <section id="contacts">
-    
+
   </section>
 `;
